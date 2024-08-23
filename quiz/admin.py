4851744643e14963b5775cf7106137c409639a50
prepa <patrickprenice@gmail.com>
2024-08-23 19:55:03 -0400

    New design and Add image

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,7 +1,7 @@
 
 
 from django.contrib import admin
-from .models import Question, Answer, QuizConfig, QuizSettings
+from .models import ImageSettings, Question, Answer, QuizConfig, QuizSettings
 
 class AnswerInline(admin.TabularInline):
     model = Answer
@@ -12,3 +12,4 @@ class QuestionAdmin(admin.ModelAdmin):
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(QuizConfig)
 admin.site.register(QuizSettings)
+admin.site.register(ImageSettings)
--- quiz/models.py
+++ quiz/models.py
@@ -2,8 +2,9 @@
 
 # Create your models here.
 from django.db import models
+from PIL import Image
 
-from django.db import models
+
 
 class QuizConfig(models.Model):
     num_questions = models.PositiveIntegerField(default=50)
@@ -14,9 +15,23 @@ class QuizConfig(models.Model):
 
 class Question(models.Model):
     text = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='question_images/', null=True, blank=True)
 
     def __str__(self):
         return self.text
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if self.image:
+            # Get the first ImageSettings object or create one with default values
+            settings, created = ImageSettings.objects.get_or_create(
+                defaults={'width': 100, 'height': 100}
+            )
+            img = Image.open(self.image.path)
+            img = img.resize((settings.width, settings.height))
+            img.save(self.image.path)
+
+    
 
 class Answer(models.Model):
     question = models.ForeignKey(Question, related_name='answers', on_delete=models.CASCADE)
@@ -31,3 +46,10 @@ class QuizSettings(models.Model):
 
     def __str__(self):
         return f"Quiz Settings (Time limit: {self.time_limit} minutes)"
+
+class ImageSettings(models.Model):
+    width = models.PositiveIntegerField(default=100)
+    height = models.PositiveIntegerField(default=100)
+
+    def __str__(self):
+        return f"Image Size: {self.width}x{self.height}"
