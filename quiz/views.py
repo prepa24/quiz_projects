fce5ce65b762caee03585bf7837d2cf5779da0dc
prepa <patrickprenice@gmail.com>
2024-08-30 20:49:45 -0400

    Add more design

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from .models import Question, Answer, QuizConfig, QuizSettings
