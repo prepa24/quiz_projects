8888f0f29956832d08d6f964ab2aef3adb6219a3
Jean Patrick Prenis <patrickprenice@gmail.com>
2025-08-24 12:01:05 -0500

    translate allauth ht

--- quiz_project/settings.py
+++ quiz_project/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -33,7 +34,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if ENVIRONMENT == 'development':
-    DEBUG = False
+    DEBUG = True
 else:
     DEBUG = False
 
