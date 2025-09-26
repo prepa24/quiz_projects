089619a1ece3409647a8b299d503b1f18bb752e0
Jean Patrick Prenis <patrickprenice@gmail.com>
2025-09-26 15:26:36 -0500

    design forms profile

--- p_users/forms.py
+++ p_users/forms.py
@@ -5,4 +5,9 @@ from .models import UserProfile
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        fields = ['image', 'phone_number', 'bio']
+        fields = ['phone_number', 'bio', 'image',]
+        widgets = {
+            "phone_number": forms.TextInput(attrs={"class": "form-control", "placeholder": "Enter US phone number"}),
+            "bio": forms.Textarea(attrs={"class": "form-control", "rows": 3}),
+            "image": forms.ClearableFileInput(attrs={"class": "form-control"}),
+        }
